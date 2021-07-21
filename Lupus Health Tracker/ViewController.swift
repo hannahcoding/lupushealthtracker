883b47228eafda810f3ef4e3f5032a5824199e52
Hannah Zacharia <hannahzacharia10@gmail.com>
2021-07-21 12:28:51 -0500

    Initial Commit

---
+++ Lupus Health Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lupus Health Tracker
+//
+//  Created by Hannah  Zacharia  on 7/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
